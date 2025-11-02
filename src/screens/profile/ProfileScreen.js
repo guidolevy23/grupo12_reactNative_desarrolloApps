7fa67593478237dbe3733349d15f93ce08bf8fdd
gydanel <alan.gagliero@gmail.com>
2025-11-02 11:01:42 -0300

    Add button style

--- src/screens/profile/ProfileScreen.js
+++ src/screens/profile/ProfileScreen.js
@@ -1,6 +1,8 @@
 import { View } from "react-native";
-import { Text } from "react-native-paper";
+import { Text, Button } from "react-native-paper";
 import styles from './styles';
+import { AuthContext } from "../../context/AuthContext";
+import { useContext } from "react";
 
 export default function ProfileScreen() {
 
@@ -9,11 +11,11 @@ export default function ProfileScreen() {
     const handleLogout = async () => {    
         await logout();
     };
-    
+
     return (
         <View style={styles.container}>
         <Text style={styles.title}>ProfileScreen</Text>
-        <Button style={styles.button} onPress={handleLogout}>
+        <Button mode="contained" onPress={handleLogout}>
             Cerrar Session
         </Button>
         </View>
